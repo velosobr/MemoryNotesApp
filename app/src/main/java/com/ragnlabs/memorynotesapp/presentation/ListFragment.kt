83f1f4c1e58d75f06d0e8b680430551538272227
velosobr <linoc.veloso@gmail.com>
2022-07-01 16:03:09 -0300

    Navigation started

---
+++ app/src/main/java/com/ragnlabs/memorynotesapp/presentation/ListFragment.kt
@@ -0,0 +1,19 @@
+package com.ragnlabs.memorynotesapp.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.ragnlabs.memorynotesapp.R
+
+
+class ListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+}
